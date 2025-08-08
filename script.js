3a8cf178f793e470d270fb7569d7a6695f698a01
mineanddani <dmartinezm.2097@gmail.com>
2025-08-08 11:10:39 -0600

    Se actualiza fecha de reloj

--- script.js
+++ script.js
@@ -127,7 +127,7 @@
 
     var textAnimate = eval(Jscex.compile("async", function () {
         var together = new Date();
-        together.setFullYear(2025, 5, 6);
+        together.setFullYear(2025, 2, 5);
         together.setHours(0);
         together.setMinutes(0);
         together.setSeconds(0);
